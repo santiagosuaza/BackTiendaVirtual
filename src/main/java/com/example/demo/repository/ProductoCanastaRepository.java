0ff6d54b1ae59a536c9bd902d2adcc02d82db975
sandos12@hotmail.com <sandos12@hotmail.com>
2020-03-31 20:46:11 -0500

    correccion metodo rep

--- src/main/java/com/example/demo/repository/ProductoCanastaRepository.java
+++ src/main/java/com/example/demo/repository/ProductoCanastaRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.model.ProductoCanasta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductoCanastaRepository extends JpaRepository<ProductoCanasta, Long> {
 }
